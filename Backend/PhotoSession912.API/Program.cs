688affb2143db4343caf380842768355b7f3e0b0
Sarahi <sarahicalix115@gmail.com>
2026-09-24 15:47:33 -0600

    Configura infraestructura de base de datos e ignora archivos .env

---
+++ Backend/PhotoSession912.API/Program.cs
@@ -0,0 +1,8 @@
+﻿using DotNetEnv; // Agregar este using
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Cargar variables de entorno desde el archivo .env local
+Env.Load();
+
+// El resto de la configuración...
